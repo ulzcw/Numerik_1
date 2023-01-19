64bb79315c273d5854a911e50e2d2f0bfa62ac40
ulzcw <ulzcw@student.kit.edu>
2023-01-19 13:59:42 +0100

    Blatt5 update

--- 5/test_kaczmarz_gs.py
+++ 5/test_kaczmarz_gs.py
@@ -1,12 +1,10 @@
-from unittest import main, TestCase
-from typing import Iterable
 from kaczmarz_gs import kaczmarz_gs
 
 import numpy as np
 import random
 
 
-def gen(dim: (int, int) = None):
+def generate_matrices(dim: (int, int) = None):
     if dim is None:
         M = random.randint(16, 26)
         N = random.randint(5, 15)
@@ -19,9 +17,22 @@ def gen(dim: (int, int) = None):
 
 
 def test_kaczmarz_gs():
-    for M in range(16, 27):
-        for N in range(5, 15):
-            print(f"({M}, {N}):", kaczmarz_gs(*gen((M, N)), np.zeros(shape=(N,)))[0])
+    lst = [("N", "average_k / N", "average_k")]
+    for N in range(5, 15):
+        average_k = 0
+        for M in range(16, 27):
+            _k = 0
+            max_iterations = 100
+
+            for _ in range(max_iterations):
+                _k += kaczmarz_gs(*generate_matrices((M, N)), np.zeros(shape=(N,)))[0]
+
+            _k /= max_iterations
+            average_k += _k
+            print(f"N = {N}, M = {M}:", _k)
+        average_k /= 10
+        lst.append((N, average_k/N, average_k))
+    print("", *lst, sep="\n")
 
 
 # class TestMatrixDecomposition(TestCase):
